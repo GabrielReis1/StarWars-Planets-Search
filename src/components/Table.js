a1061151751dd711dd6c40aea23681a7ada8c8fe
GabrielReis1 <gabrielreis.santos@outlook.com>
2023-04-02 18:52:38 -0300

    requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { FilterContext } from '../context/FilterContext';
 import FilterInput from './FilterInput';
+import NumericFilter from './NumericFilter';
 import '../styles/table.css';
 
 function Table({ data }) {
@@ -9,12 +10,31 @@ function Table({ data }) {
     ? Object.keys(data[0]).map((header) => <th key={ header }>{header}</th>)
     : null;
 
-  const { filter } = useContext(FilterContext);
+  const { filter, numericFilter } = useContext(FilterContext);
 
   const filteredData = data.filter((planet) => planet.name.toLowerCase()
     .includes(filter.toLowerCase()));
 
-  const rows = filteredData.map((planet, index) => (
+  const rows = filteredData.filter((planet) => {
+    const { column } = numericFilter;
+    const { comparison } = numericFilter;
+    const value = Number(numericFilter.value);
+
+    if (!column || !comparison || Number.isNaN(value)) {
+      return true;
+    }
+
+    switch (comparison) {
+    case 'maior que':
+      return Number(planet[column]) > value;
+    case 'menor que':
+      return Number(planet[column]) < value;
+    case 'igual a':
+      return Number(planet[column]) === value;
+    default:
+      return true;
+    }
+  }).map((planet, index) => (
     <tr key={ index }>
       {Object.values(planet).map((value, i) => (
         <td key={ i }>{value}</td>
@@ -25,6 +45,7 @@ function Table({ data }) {
   return (
     <div>
       <FilterInput />
+      <NumericFilter />
       <div className="table-container">
         <table>
           <thead>
